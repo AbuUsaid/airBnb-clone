6c7d3eb842e392b0d2df3bb885a69ca7fbf49dff
AbuUsaid <usaidabu321@gmail.com>
2023-11-09 18:03:16 +0530

    Create Navbar and add icon to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css';
+import './style.css';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Navbar />
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import logo from '../images/airbnb_icon.png';
+const Navbar = () => {
+  return (
+    <nav>
+      <img className="airbnb-icon" src={logo} />
+    </nav>
+  );
+};
+
+export default Navbar;
