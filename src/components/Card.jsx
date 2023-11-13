c133579612fa5b254b53c565f752b7b2ba1f1e4a
AbuUsaid <usaidabu321@gmail.com>
2023-11-13 23:34:48 +0530

    pass object as a prop

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,24 +4,24 @@ import star from '../images/Star.png';
 
 const Card = (props) => {
   let badgeText;
-  if (props.openSpots === 0) {
+  if (props.item.openSpots === 0) {
     badgeText = 'SOLD OUT';
-  } else if (props.location === 'Online') {
+  } else if (props.item.location === 'Online') {
     badgeText = 'ONLINE';
   }
 
   return (
     <div className="card">
       {badgeText && <div className="card--badge">{badgeText}</div>}
-      <img src={`src/images/${props.img}`} className="card--image" />
+      <img src={`src/images/${props.item.coverImg}`} className="card--image" />
       <div className="card--stats">
         <img src={star} className="card--star" />
-        <span>{props.rating}</span>
-        <span className="gray">({props.reviewCount}) • </span>
-        <span className="gray">{props.location}</span>
+        <span>{props.item.stats.rating}</span>
+        <span className="gray">({props.item.stats.reviewCount}) • </span>
+        <span className="gray">{props.item.location}</span>
       </div>
-      <p>{props.title}</p>
-      <p>From ${props.price} / person</p>
+      <p>{props.item.title}</p>
+      <p>From ${props.item.price} / person</p>
     </div>
   );
 };
